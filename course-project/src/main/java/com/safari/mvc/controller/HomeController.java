458f75bfc07175c893e37eb1f60b4fe22ac4d3ef
RenanChagas <renanchagasw@gmail.com>
2016-05-22 12:00:01 -0300

    23.203
    Adding Services and Autowired

--- course-project/src/main/java/com/safari/mvc/controller/HomeController.java
+++ course-project/src/main/java/com/safari/mvc/controller/HomeController.java
@@ -1,13 +1,23 @@
 package com.safari.mvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import com.safari.mvc.model.Project;
 
 @Controller
 public class HomeController {
 	
-	@RequestMapping("/home")
-	public String goHome(){
+	@RequestMapping("/")
+	public String goHome(Model model){
+		
+		Project project = new Project();
+		project.setName("First Project");
+		project.setSponsor("Nasa");
+		project.setDescription("This is a simple project sponsored by NASA");
+		
+		model.addAttribute("currentProject", project);
+		
 		return "home";
 	}
 
