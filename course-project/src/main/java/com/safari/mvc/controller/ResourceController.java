87c8b35459c6cf874db03f50e41b4583242c8ee9
RenanChagas <renanchagasw@gmail.com>
2016-05-26 23:14:08 -0300

    70.203 - MVC End
    Spring MVC end course

--- course-project/src/main/java/com/safari/mvc/controller/ResourceController.java
+++ course-project/src/main/java/com/safari/mvc/controller/ResourceController.java
@@ -1,19 +1,27 @@
 package com.safari.mvc.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
 import com.safari.mvc.model.Resource;
+import com.safari.mvc.service.ResourceService;
 
 @Controller
 @RequestMapping("/resource")
 @SessionAttributes("resource")
 public class ResourceController {
+	
+	@Autowired
+	private ResourceService resourceService;
+	
 	
 	@RequestMapping(value="/add", method=RequestMethod.GET)
 	public String add(Model model){
@@ -33,5 +41,19 @@ public class ResourceController {
 		status.setComplete();
 		return "resource";
 	}
+	
+	@RequestMapping("/find")
+	public String find(Model model){
+		model.addAttribute("resources", resourceService.findAll());
+		return "resources";
+	}
+	
+	@RequestMapping("/{resourceId}")
+	@ResponseBody
+	public Resource findResource(@PathVariable("resourceId") Long resourceId){
+		return resourceService.find(resourceId);
+	}
+	
+	
 	
 }
--- course-project/src/main/java/com/safari/mvc/model/Resource.java
+++ course-project/src/main/java/com/safari/mvc/model/Resource.java
@@ -1,6 +1,7 @@
 package com.safari.mvc.model;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 
 public class Resource {
 
@@ -13,6 +14,23 @@ public class Resource {
 	private BigDecimal cost;
 
 	private String unitOfMeasure;
+
+	private String[] indicators;
+
+	private String notes;
+
+	public Resource() {
+		super();
+	}
+
+	public Resource(Long resourceId, String name, String type, BigDecimal cost,
+			String unitOfMeasure) {
+		this.resourceId = resourceId;
+		this.name = name;
+		this.type = type;
+		this.cost = cost;
+		this.unitOfMeasure = unitOfMeasure;
+	}
 
 	public Long getResourceId() {
 		return resourceId;
@@ -54,11 +72,28 @@ public class Resource {
 		this.unitOfMeasure = unitOfMeasure;
 	}
 
+	public String[] getIndicators() {
+		return indicators;
+	}
+
+	public void setIndicators(String[] indicators) {
+		this.indicators = indicators;
+	}
+
+	public String getNotes() {
+		return notes;
+	}
+
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+
 	@Override
 	public String toString() {
 		return "Resource [resourceId=" + resourceId + ", name=" + name
 				+ ", type=" + type + ", cost=" + cost + ", unitOfMeasure="
-				+ unitOfMeasure + "]";
+				+ unitOfMeasure + ", indicators=" + Arrays.toString(indicators)
+				+ ", notes=" + notes + "]";
 	}
 
 }
